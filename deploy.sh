cb318a7debdfcdc4254313c8600888618ab8e0be
Sudarshan <sudarshan@Madhuris-MacBook-Air.local>
2022-09-12 23:40:40 +0530

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+cd ~
+mkdir git_sud
+cd git_sud
+git clone https://github.com/sudarshansudarshan/nivedita
+
+git clone https://github.com/sudarshansudarshan/shahid
+
+git clone https://github.com/sudarshansudarshan/harsh
+
+git clone https://github.com/sudarshansudarshan/ashu
+git clone https://github.com/sudarshansudarshan/jayant
+
+git clone https://github.com/sudarshansudarshan/akhil_arya
+git clone https://github.com/sudarshansudarshan/btp_vocabproj
+git clone https://github.com/sudarshansudarshan/cs522
+git clone https://github.com/sudarshansudarshan/plakshaprog
+git clone https://github.com/sudarshansudarshan/plakshamath
+git clone https://github.com/sudarshansudarshan/codershigh
+git clone https://github.com/sudarshansudarshan/sudarshansudarshan.github.io
+git clone https://github.com/sudarshansudarshan/btp_kdap
+git clone https://github.com/sudarshansudarshan/personal
+git clone https://github.com/sudarshansudarshan/sudvim
+git clone https://github.com/sudarshansudarshan/underthetree
+git clone https://github.com/sudarshansudarshan/gurukul
+
+
+
